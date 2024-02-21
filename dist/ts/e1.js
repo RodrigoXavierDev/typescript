4068796ae8fd166fba2d627b75d5bb211101d5be
Rodrigo Xavier <141236357+RodrigoXavierDev@users.noreply.github.com>
2024-02-21 11:52:34 -0300

    atividade de saída de dados

--- dist/ts/e1.js
+++ dist/ts/e1.js
@@ -12,6 +12,14 @@ class Funcionario {
             this.cargo = cargo,
             this.data_nascimento = data_nascimento;
     }
+    mostrarDados() {
+        console.log(`nome do funcionario: ${this.nome}`);
+        console.log(`email: ${this.email}`);
+        console.log(`telefone: ${this.telefone}`);
+        console.log(`cargo: ${this.cargo}`);
+        console.log(`Data de nascimento: ${this.data_nascimento}`);
+        console.log(`___________________________________________`);
+    }
 }
 class Paciente {
     nome;
@@ -27,6 +35,15 @@ class Paciente {
             this.idade = idade,
             this.cpf = cpf,
             this.data_nascimento = data_nascimento;
+    }
+    mostrarDados() {
+        console.log(`nome do paciente: ${this.nome}`);
+        console.log(`telefone do paciente: ${this.telefone}`);
+        console.log(`sintomas: ${this.sintomas}`);
+        console.log(`idade: ${this.idade}`);
+        console.log(`cpf: ${this.cpf}`);
+        console.log(`data de nascmento: ${this.data_nascimento}`);
+        console.log(`___________________________________________`);
     }
 }
 class Consulta {
@@ -44,10 +61,18 @@ class Consulta {
             this.data = data,
             this.convenio = convenio;
     }
+    mostraDados() {
+        console.log(`Hospital da consulta: ${this.hospital}`);
+        console.log(`Local do hospital: ${this.localicacao}`);
+        console.log(`Medico resposavel: ${this.medico}`);
+        console.log(`Paciente atendido: ${this.paciente}`);
+        console.log(`Data do atendimento: ${this.data}`);
+        console.log(`Paciente com convenio? ${this.convenio}`);
+    }
 }
 const funcionario1 = new Funcionario('Fulano', 'fulano@gmail.com', '84999999999', 'medico', new Date("2023-02-20"));
 const paciente1 = new Paciente('silcrano', '84987654321', 'tosse', '18', '77777777777', new Date("2023-02-20"));
 const consulta1 = new Consulta('Hospital das dores', 'Rua das curas', funcionario1, paciente1, new Date("2023-02-20"), true);
-console.log(paciente1);
-console.log(funcionario1);
-console.log(consulta1);
+console.log(paciente1.mostrarDados());
+console.log(funcionario1.mostrarDados);
+console.log(consulta1.mostraDados());
--- src/ts/e2.ts
+++ src/ts/e2.ts
@@ -13,7 +13,16 @@ class Livro {
         this.data_publicacao = data_publicacao,
         this.estoque = estoque
     }
-}
+    mostrarDados() {
+        console.log(`Titulo do livro: ${this.titulo}`)
+        console.log(`Autor: ${this.autor}`)
+        console.log(`Editora: ${this.editora}`)
+        console.log(`Categoria: ${this.categoria}`)
+        console.log(`Data de publicação: ${this.data_publicacao}`)
+        console.log(`Estoque: ${this.estoque}`)
+        console.log(`___________________________________________`)
+    }
+} 
 
 class Usuario {
     nome: string;
@@ -25,6 +34,13 @@ class Usuario {
         this.endereco = endereco,
         this.telefone = telefone,
         this.cpf = cpf
+    }
+    mostrarDados() {
+        console.log(`Nome do usuario: ${this.nome}`)
+        console.log(`Endereco: ${this.endereco}`)
+        console.log(`Telefone: ${this.telefone}`)
+        console.log(`CPF: ${this.cpf}`)
+        console.log(`___________________________________________`)
     }
 }
 
@@ -39,12 +55,18 @@ class Emprestimo {
         this.info_usuario = info_usuario,
         this.info_livro = info_livro
     }
+    mostrarDados() {
+        console.log(`Data de empretimo: ${this.data_emprestimo}`)
+        console.log(`Data de devolução: ${this.data_devolucao}`)
+        console.log(`Informações do usuario: ${this.info_usuario}`)
+        console.log(`Informações do livro${this.info_livro}`)
+    }
 }
 
 const livro1 = new Livro('livro bom', 'fulano', 'editora boa', 'livro de terror', new Date("2005-05-05"), 50)
 const usuario1 = new Usuario('silcrano', 'rua do além', '84987654321', '77777777777')
 const emprestimo1 = new Emprestimo(new Date("2005-05-05"), new Date("2023-02-21"), usuario1, livro1)
 
-console.log(livro1)
-console.log(usuario1)
-console.log(emprestimo1)
+console.log(livro1.mostrarDados())
+console.log(usuario1.mostrarDados())
+console.log(emprestimo1.mostrarDados())
